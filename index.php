e03c67239574201ed54022e79a66a0c067ddd4e7
Kophsbsj <phisidsaengdala@gmail.com>
2024-09-28 12:55:02 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include_once('header.php'); ?>
+
+<?php include_once('nav.php'); ?>
+
+<div class="container my-3">
+    <h1 class="display-4">phisit</h1>
+    <hr>
+    <p></p>
+</div>
+
+<?php include_once('footer.php'); ?>
